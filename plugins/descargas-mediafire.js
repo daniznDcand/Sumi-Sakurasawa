78cdc5236cd966e124d25eadf9f8e760a3cf77d7
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-15 09:04:05 -0500

    💙HATSUNE MIKU💙

--- plugins/descargas-mediafire.js
+++ plugins/descargas-mediafire.js
@@ -35,7 +35,7 @@ const handler = async (m, {conn, args, usedPrefix, command}) => {
     }
   }
 };
-handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i;
+handler.command = /^(mediafire|mediafiredl|dlmediafire|mf)$/i;
 export default handler;
 
 async function mediafireDl(url) {
