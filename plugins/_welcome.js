c6f65743c08fcfd81a6546a20c1c362a9234d922
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-26 12:49:57 -0500

    💙HATSUNE MIKU💙

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -2,26 +2,72 @@ import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, { conn, participants, groupMetadata }) {
-  if (!m.messageStubType || !m.isGroup) return !0;
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://files.catbox.moe/wm4w1x.jpg')
-  let img = await (await fetch(`${pp}`)).buffer()
-  let chat = global.db.data.chats[m.chat]
-  let txt = '💙 ¡Nueva Estrella Virtual! 💙'
-  let txt1 = '🎵 ¡Sayonara! 🎵'
-  let groupSize = participants.length
-  if (m.messageStubType == 27) {
-    groupSize++;
-  } else if (m.messageStubType == 28 || m.messageStubType == 32) {
-    groupSize--;
+  if (!m.messageStubType || !m.isGroup) return true;
+  
+  const fkontak = {
+    "key": {
+      "participants": "0@s.whatsapp.net",
+      "remoteJid": "status@broadcast",
+      "fromMe": false,
+      "id": "Halo"
+    },
+    "message": {
+      "contactMessage": {
+        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+      }
+    },
+    "participant": "0@s.whatsapp.net"
+  };
+
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(() => 'https://files.catbox.moe/wm4w1x.jpg');
+  let img = await (await fetch(pp)).buffer();
+  let chat = global.db.data.chats[m.chat];
+  
+  let groupSize = participants.length;
+  if (m.messageStubType === 27) groupSize++;
+  else if (m.messageStubType === 28 || m.messageStubType === 32) groupSize--;
+
+  if (chat.welcome && m.messageStubType === 27) {
+    
+    let welcomeMsg = `
+👋 ¡Hola @${m.messageStubParameters[0].split('@')[0]}!
+
+Bienvenido a *${groupMetadata.subject}* 🎉
+
+Somos ya *${groupSize}* fanáticos de Miku que te reciben con mucha emoción.
+
+🎤 ${global.welcom1}
+
+Prepárate para disfrutar y compartir momentos geniales aquí con nosotros.
+
+Para cualquier ayuda, escribe *#help*.
+
+No olvides visitar nuestro canal:
+https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o
+
+¡Que la música te acompañe siempre! 🎶
+    `;
+    await conn.sendMini(m.chat, '💙 ¡Nueva estrella se une! 💙', dev, welcomeMsg, img, img, redes, fkontak, m, rcanal);
   }
 
-  if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `🎤 *¡Konnichiwa!* Bienvenido al concierto virtual de ${groupMetadata.subject}🎤\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎵 ${global.welcom1} 🎵\n\n🌟 ¡Ahora somos ${groupSize} fanáticos de Miku! 🌟\n\n🎶 ¡Prepárate para cantar con nosotros! (◕‿◕)♡ 🎶\n\n> 🎤 Usa *#help* para ver todos los comandos mágicos de Miku! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> 乃ㄚ 乃ᐯ廾㇌ 工𝓝ᗪㄩ丂ㄒ尺工㠪丂`    
-    await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak, m, rcanal)
+  if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    
+    let byeMsg = `
+👋 ¡Hasta luego @${m.messageStubParameters[0].split('@')[0]}!
+
+Te extrañaremos en *${groupMetadata.subject}*.
+
+🎤 ${global.welcom2}
+
+Ahora somos *${groupSize}* y esperamos que regreses pronto.
+
+La música de Miku seguirá sonando fuerte aquí para ti.
+
+No olvides seguir nuestro canal:
+https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o
+
+¡Cuídate y hasta el próximo concierto! 🎶✨
+    `;
+    await conn.sendMini(m.chat, '🎵 ¡Sayonara! 🎵', dev, byeMsg, img, img, redes, fkontak, m, rcanal);
   }
-  
-  if (chat.welcome && (m.messageStubType == 28 || m.messageStubType == 32)) {
-    let bye = `🎵 *¡Sayonara!* ${groupMetadata.subject} te extrañará 🎵\n\n💙 @${m.messageStubParameters[0].split`@`[0]} 💙\n\n🎤 ${global.welcom2} 🎤\n\n🌟 Ahora somos ${groupSize} fanáticos esperándote 🌟\n\n🎶 ¡Esperamos verte pronto en nuestro próximo concierto! (｡◕‿◕｡) 🎶\n\n> 🎵 ¡La música de Miku siempre te acompañará! ✨\n SIGUE NUESTRO CANAL \n https://www.whatsapp.com/channel/0029VajYamSIHphMAl3ABi1o\n> 乃ㄚ 乃ᐯ廾㇌ 工𝓝ᗪㄩ丂ㄒ尺工㠪丂`
-    await conn.sendMini(m.chat, txt1, dev, bye, img, img, redes, fkontak, m, rcanal)
-  }}
+}
