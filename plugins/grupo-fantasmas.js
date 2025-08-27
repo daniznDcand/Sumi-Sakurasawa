af8b48eb82034d2525c2f9db4a60a22c2cc0d22e
DEPOOL <145615059+Brauliovh3@users.noreply.github.com>
2025-08-27 08:36:46 -0500

    💙HATSUNE MIKU💙

--- plugins/grupo-fantasmas.js
+++ plugins/grupo-fantasmas.js
@@ -1,16 +1,18 @@
 import { areJidsSameUser } from '@whiskeysockets/baileys'
 
-var handler = async (m, { conn, text, participants, args, command }) => {
+var handler = async (m, { conn, text, participants, command }) => {
+  
   let member = participants.map(u => u.id)
-  let sum = (!text) ? member.length : text
+  let sum = (!text) ? member.length : text 
   var total = 0
   var sider = []
 
+  
   for (let i = 0; i < sum; i++) {
     let users = m.isGroup ? participants.find(u => u.id == member[i]) : {}
     if ((typeof global.db.data.users[member[i]] == 'undefined' || global.db.data.users[member[i]].chat == 0) && !users.isAdmin && !users.isSuperAdmin) {
       if (typeof global.db.data.users[member[i]] !== 'undefined') {
-        if (global.db.data.users[member[i]].whitelist == false) {
+        if (!global.db.data.users[member[i]].whitelist) {
           total++
           sider.push(member[i])
         }
@@ -21,25 +23,27 @@ var handler = async (m, { conn, text, participants, args, command }) => {
     }
   }
 
+  
   const delay = time => new Promise(res => setTimeout(res, time))
 
   switch (command) {
     case 'fantasmas':
       if (total == 0) return conn.reply(m.chat, `${emoji} Este grupo es activo, no tiene fantasmas.`, m)
-      m.reply(`${emoji} *Revisión de inactivos*\n\n${emoji2} *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:*\nEsto no es al 100% acertado, el bot inicia el conteo de mensajes a partir de que se active en este número`, null, { mentions: sider })
+      m.reply(`${emoji} *Revisión de inactivos*\n\n${emoji2} *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n*📝 NOTA:*\nEsto no es 100% exacto, el bot cuenta mensajes desde que se activa.`, null, { mentions: sider })
       break
 
     case 'kickfantasmas':
-      if (total == 0) return conn.reply(m.chat, `${emoji} Este grupo es activo no tiene fantasmas.`, m)
-      await m.reply(`${emoji} *Eliminación de inactivos*\n\n${emoji2} *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n${msm} _*El bot eliminará a los usuarios de la lista mencionada cada 10 segundos.*_`, null, { mentions: sider })
-      await delay(1 * 10000)
+      if (total == 0) return conn.reply(m.chat, `${emoji} Este grupo es activo, no tiene fantasmas.`, m)
+      await m.reply(`${emoji} *Eliminación de inactivos*\n\n${emoji2} *Lista de fantasmas*\n${sider.map(v => '@' + v.replace(/@.+/, '')).join('\n')}\n\n_El bot eliminará a estos usuarios, uno cada 10 segundos._`, null, { mentions: sider })
+      await delay(10000)
       let chat = global.db.data.chats[m.chat]
       chat.welcome = false
       try {
         for (let user of sider) {
+          
           if (user.endsWith('@s.whatsapp.net') && !(participants.find(v => areJidsSameUser(v.id, user)) || { admin: true }).admin) {
             await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-            await delay(1 * 10000)
+            await delay(10000)
           }
         }
       } finally {
@@ -57,3 +61,4 @@ handler.admin = true
 handler.fail = null
 
 export default handler
+
