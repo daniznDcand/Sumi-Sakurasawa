fd6150e687319bd915a374da44c8f8e200349540
Brauliovh3 <velasquezhuillcab@gmail.com>
2025-09-29 11:04:22 -0500

    💙HATSUNE MIKU💙

--- plugins/anime-eat.js
+++ plugins/anime-eat.js
@@ -16,23 +16,17 @@ let handler = async (m, { conn, usedPrefix }) => {
         : `\`${name2}\` está comiendo en el mundo virtual (っ˘ڡ˘ς) ✨🎵`
     
     if (m.isGroup) {
-        let pp = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863608402.mp4'
-        let pp2 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863601845.mp4'
-        let pp3 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863654803.mp4'
-        let pp4 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863646010.mp4'
-        let pp5 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863636034.mp4'
-        let pp6 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863624765.mp4'
-        let pp7 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863597122.mp4'
-        let pp8 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863743960.mp4'
-        let pp9 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863731956.mp4'
-        let pp10 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863726838.mp4'
-        let pp11 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863692870.mp4'
-        let pp12 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1742863676967.mp4'
-        let pp13 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1745602892981.mp4'
-        let pp14 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1745602901923.mp4'
-        let pp15 = 'https://raw.githubusercontent.com/Brauliovh3/HATSUNE-MIKU/main/Contenido/1745602887999.mp4'
+        let pp = 'https://media.tenor.com/ZBO4pyuseVcAAAPo/engage-kiss-kanna.mp4'
+        let pp2 = 'https://media.tenor.com/uk9xO0xpWoIAAAPo/burger-eating.mp4'
+        let pp3 = 'https://media.tenor.com/rL9bf5nEuFUAAAPo/dragon-ball-eating.mp4'
+        let pp4 = 'https://media.tenor.com/51l2Vl3A9ZcAAAPo/zombielandsaga-eat.mp4'
+        let pp5 = 'https://media.tenor.com/c1l2eqy7SJgAAAPo/sasuke-uchiha-eating.mp4'
+        let pp6 = 'https://media.tenor.com/a2sQtVpp8EYAAAPo/she-blushes-with-her-eyes-close-and-speaking-while-eating.mp4'
+        let pp7 = 'https://media.tenor.com/EqqzgcETPSoAAAPo/shiroha-naruse.mp4'
+        let pp8 = 'https://media.tenor.com/SdQXVzloThEAAAPo/chewzuka-cheeks.mp4'
         
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15]
+        
+        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8]
         const video = videos[Math.floor(Math.random() * videos.length)]
         
         conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, ptt: true, mentions: [who] }, { quoted: m })
@@ -41,7 +35,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 
 handler.help = ['eat']
 handler.tags = ['anime']
-handler.command = ['eat', 'comer']
+handler.command = ['eat', 'comer','almorzar','cenar','desayunar','food','comida','snack','tragar','devorar','digerir','masticar','engullir','zampar']
 handler.group = true
 
 export default handler
