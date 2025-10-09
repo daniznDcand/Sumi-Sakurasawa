31cc587f4d04c3600178976eeccf78dbcd565939
Brauliovh3 <velasquezhuillcab@gmail.com>
2025-10-08 19:08:34 -0600

    💙HATSUNE MIKU💙

--- plugins/info-depool.js
+++ plugins/info-depool.js
@@ -7,22 +7,6 @@ let handler = async (m, { conn }) => {
 
 ¡Gracias por usar el bot! Si quieres apoyar el proyecto, puedes hacerlo con un donativo. Tu ayuda permite que el bot siga activo y mejorando.
 `;
-
-  const buttons = [
-    [
-      {
-        buttonId: '.apoyardepool',
-        buttonText: { displayText: '💙 Apoyar' },
-        type: 1
-      },
-      {
-        buttonId: '.sabermasdepool',
-        buttonText: { displayText: 'ℹ️ Saber más' },
-        type: 1
-      }
-    ]
-  ];
-
   await conn.sendMessage(m.chat, {
     text: descripcion,
     footer: '🌱 Gracias por tu apoyo',
@@ -33,24 +17,22 @@ let handler = async (m, { conn }) => {
     headerType: 1
   }, { quoted: m });
 };
-
 handler.command = ['depool'];
 handler.help = ['depool'];
 handler.tags = ['info'];
+export default handler;
 
 
-let handlerApoyar = async (m, { conn }) => {
+export async function apoyardepool(m, { conn }) {
   await conn.sendMessage(m.chat, {
     image: { url: 'https://i.postimg.cc/JnVS0C1z/yape.jpg' },
     caption: '💙 ¡Gracias por tu apoyo! Escanea el QR para donar vía Yape.'
   }, { quoted: m });
-};
-handlerApoyar.command = ['apoyardepool'];
+}
+apoyardepool.command = ['apoyardepool'];
 
 
-let handlerSaberMas = async (m, { conn }) => {
+export async function sabermasdepool(m, { conn }) {
   await conn.reply(m.chat, 'Puedes contactarme en GitHub: https://github.com/Brauliovh3 o en WhatsApp para más información.', m);
-};
-handlerSaberMas.command = ['sabermasdepool'];
-
-export default [handler, handlerApoyar, handlerSaberMas];
+}
+sabermasdepool.command = ['sabermasdepool'];
