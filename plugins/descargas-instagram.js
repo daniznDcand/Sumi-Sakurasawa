4c2656d3cb3515f242d7ea26ab93ccbb9318eea9
Brauliovh3 <velasquezhuillcab@gmail.com>
2025-11-14 10:16:39 -0500

    💙HATSUNE MIKU💙

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -2,7 +2,7 @@ import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { args, conn }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, `🎤💙 Por favor, ingresa un enlace de Instagram para descargarlo en el mundo virtual ✨🎵`, m);
+    return conn.reply(m.chat, `💙 Por favor, ingresa un enlace de Instagram para descargarlo en el mundo virtual 🎵`, m, rcanal);
   }
 
   try {
@@ -11,12 +11,12 @@ const handler = async (m, { args, conn }) => {
     const data = res.data;
 
     for (let media of data) {
-      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `🎤💙 ¡Aquí tienes tu descarga del concierto virtual! ✨🎵💫`, m);
-    await m.react('✨');
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `💙 ¡Aquí tienes tu descarga del concierto virtual! 🎵`, m,rcanal);
     }
+    await m.react('✨');
   } catch (e) {
-    return conn.reply(m.chat, `🎤💙 ¡Gomen! Ocurrió un error en el mundo virtual. ¡Inténtalo de nuevo! ✨💫`, m);
     await m.react('💙');
+    return conn.reply(m.chat, `💙 ¡Gomen! Ocurrió un error en el mundo virtual. ¡Inténtalo de nuevo! 💫`, m, rcanal);
   }
 };
 
