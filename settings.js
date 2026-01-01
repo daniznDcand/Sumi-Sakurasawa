2d6c331e0a2d28e243315cdb331d0064ac6027a5
Brauliovh3 <velasquezhuillcab@gmail.com>
2026-01-01 17:13:42 -0600

    💙HATSUNE MIKU💙

--- settings.js
+++ settings.js
@@ -119,7 +119,7 @@ global.emoji3 = '💖'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363401404146384@newsletter',
+ch1: '120363315369913363@newsletter',
 }
 global.multiplier = 60
 
